8d2594685fe404891744cda523baf7666938d1c6
Wai Hwa Tan <twaih92@gmail.com>
2015-08-20 15:04:07 -0700

    added google-map-react

--- client/components/Map.js
+++ client/components/Map.js
@@ -7,14 +7,14 @@ var Map = React.createClass({
 	getInitialState: function(){
 		return{
 			center: [33.979471, -118.422549],
-			zoom: 9,
+			zoom: 12,
 		};
 	},
 
 	render: function(){
 		return(
 		<div>
-		test50
+		test25
 		<GoogleMap center={this.state.center} zoom={this.state.zoom}>
 		</GoogleMap>
       <Feed source="https://api.instagram.com/v1/locations/search?lat=48.858844&lng=2.294351&client_id=46141b7b17fa4f29911b66e830bafcf1"/>
