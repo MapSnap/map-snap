db358c5bba9aa00cd7dfb4d9915eb42ebc9c8ac4
Wai Hwa Tan <twaih92@gmail.com>
2015-08-20 16:38:50 -0700

    resizing map + styling

--- client/components/Map.js
+++ client/components/Map.js
@@ -1,4 +1,5 @@
 var React = require('react');
+var StyleSheet = require('react-style');
 var Feed = require('./Feed');
 var GoogleMap = require('google-map-react');
 
@@ -7,20 +8,27 @@ var Map = React.createClass({
 	getInitialState: function(){
 		return{
 			center: [33.979471, -118.422549],
-			zoom: 12,
+			zoom: 15,
 		};
 	},
 
 	render: function(){
 		return(
-		<div>
-		test25
+		<div styles={styles.gmap}>
 		<GoogleMap center={this.state.center} zoom={this.state.zoom}>
 		</GoogleMap>
-      <Feed source="https://api.instagram.com/v1/locations/search?lat=48.858844&lng=2.294351&client_id=46141b7b17fa4f29911b66e830bafcf1"/>
-      test23
 		</div>
 		);
+	}
+});
+
+
+var styles =
+StyleSheet.create({
+	gmap:{
+		height: '50%',
+		width: '50%',
+		margin: '0 auto'
 	}
 });
 
