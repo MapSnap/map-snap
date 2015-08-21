dfbe87f81a99ee5aa9792155b64b25661a0bf1e8
RedWood <mackey91@gmail.com>
2015-08-20 18:44:31 -0700

    update map

--- client/components/Map.js
+++ client/components/Map.js
@@ -1,8 +1,10 @@
 var React = require('react');
+var StyleSheet = require('react-style');
 var Feed = require('./Feed');
 var GoogleMap = require('google-map-react');
 
 var Map = React.createClass({
+
   getInitialState: function(){
     return{
       center: [33.979471, -118.422549],
@@ -19,16 +21,26 @@ var Map = React.createClass({
 
   render: function(){
     var value = this.state.value;
-  	return(
-  	<div>
+    return(
+    <div>
     <GoogleMap center={this.state.center} zoom={this.state.zoom}>
     </GoogleMap>
           <input type = "text" value={value} onChange={this.handleChange}/>
         <Feed source = "https://api.instagram.com/v1/tags/nofilter/media/recent?client_id=46141b7b17fa4f29911b66e830bafcf1&callback=?"/>
-  	</div>
-  	);
+    </div>
+    );
   }
 
+
+
+var styles =
+StyleSheet.create({
+  gmap:{
+    height: '50%',
+    width: '50%',
+    margin: '0 auto'
+  }
 });
+
 
 module.exports = Map;
